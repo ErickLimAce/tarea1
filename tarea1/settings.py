423983e213eb73b8d7a0cc695119ddba8cb0f0a4
Erick389 <ericklimonace@gmail.com>
2023-03-29 11:58:15 -0600

    cambios video1

--- tarea1/settings.py
+++ tarea1/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-(x=%wjaf7s_8h=5t9t#h-uu9rzi(ausvvywox5matjm1+&o548"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "calculadora",
+    "rest_framework",
+    
 ]
 
 MIDDLEWARE = [
